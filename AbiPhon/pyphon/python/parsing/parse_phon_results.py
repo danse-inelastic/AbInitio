0fc2c346e6a75a481739343fc8ec9e54abc93fec
Olivier A. Delaire <delaireoa@ornl.gov>
2008-01-15 00:38:01 +0000

    added parser from Phon to IDF
    git-svn-id: svn://danse.us/AbInitio@20 4efde5e4-ebd3-4001-8375-eeb568285340

--- AbiPhon/pyphon/python/parsing/parse_phon_results.py
+++ AbiPhon/pyphon/python/parsing/parse_phon_results.py
@@ -52,6 +52,49 @@ def parse(filename):
         rt.append(data.asList())    
     return rt 
 
+
+def parsePhon2IDF(inputfilename='phon.out',
+                  polarizationsfile='polarizations.idf',
+                  omega2sfile='energies.idf',
+                  D=3):
+    """Parses the Phon output and writes a IDF-format file for polarization vectors.
+    D is the dimensionality for the crystal; it should be equal to 3 for Phon outputs."""
+
+    import numpy
+    from idf.Polarizations import write as writePols
+    from idf.Omega2 import write as writeOmega2s
+    
+    try:
+        infile = open(inputfilename, 'r')
+        #polfile = open(polarizationsfile, 'w')
+        #om2file = open(omega2sfile, 'w')
+    except IOError, (errno, strerror):
+        print "I/O error(%s): %s" % (errno, strerror)
+    inputString = infile.read()
+    res = []
+    for data, dataStart, dataEnd in modesAtPoint.scanString(inputString):
+        rt.append(data.asList())    
+
+    nkpts = len(res)
+    if nkpts == 0: raise ValueError, 'No phonon mode was parsed.'
+    nmodes = len(res[0])
+    natoms = nmodes / D
+    pols = numpy.zeros((nkpts, nmodes, natoms, D), dtype='float')
+    omega2s = numpy.zeros((nkpts, nmodes), dtype='float')
+    for kIndex in range(nkpts):
+        for modeIndex in range(nmodes):
+            omega2s[kIndex][modeIndex] = res[kIndex][modeIndex]
+            for atomIndex in range(natoms):
+                polvec = numpy.array([x+1j*y for (x,y) in res[kIndex][modeIndex][atomIndex]])
+                pols[kIndex,modeIndex,atomIndex] = polvec # this will crash if D's don't match
+
+    writePols(pols, filename=polarizationsfile,
+              comment='Parsed from Phon output file '+inputfilename)
+    writeOmega2s(omega2s, filename=omega2sfile,
+                 comment='Parsed from Phon output file '+inputfilename, D=3)
+    return
+
+
 def test():
     results = parse("phon.out_dos_noIBZ")
 
