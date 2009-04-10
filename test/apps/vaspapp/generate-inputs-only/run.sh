2826358cc4d9db11fb7d99c2db9015d0904fc1e5
Jiao Lin <linjiao@caltech.edu>
2009-04-10 04:55:32 +0000

    tests/apps/vaspapp/generate-inputs-only/run.sh: fixed a bug
    git-svn-id: svn://danse.us/AbInitio@116 4efde5e4-ebd3-4001-8375-eeb568285340

--- test/apps/vaspapp/generate-inputs-only/run.sh
+++ test/apps/vaspapp/generate-inputs-only/run.sh
@@ -1,1 +1,1 @@
-vaspapp.py -name="VaspCalculation_Si" -unitcell=Si.xyz -ecutoff=240 -xcf=PAW-PBE -mpmesh=21,21,21 -generateInputsOnly
+vaspapp.py -name="VaspCalculation_Si" -unitcell=Si.xyz -ecutoff=240 -xcf=pawpbe -mpmesh=21,21,21 -generateInputsOnly
