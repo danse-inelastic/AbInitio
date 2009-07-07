39298facbb7dd8266014c931fdf30a3475ea1551
Nikolay Markovskiy <markovskiy@gmail.com>
2009-07-07 04:11:01 +0000

    Deleted task directory from AbInitio/espresso
    git-svn-id: svn://danse.us/AbInitio@155 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/task/example/main.py
+++
@@ -1,8 +0,0 @@
-from espresso.task.ecutconverger import EcutConverger
-from espresso.task.kconverger import KConverger
-
-
-#k_opt = KConverger('config.ini')
-
-e_opt = EcutConverger('config.ini')
-
--- espresso/task/main.py
+++
@@ -1,8 +0,0 @@
-from ecutconverger import EcutConverger
-from kconverger import KConverger
-
-
-#k_opt = KConverger('config.ini')
-
-e_opt = EcutConverger('config.ini')
-
