8d0bad596e9ede0222008b34e80eb491796a28c9
Alexander Dementsov <dexity@gmail.com>
2009-07-19 22:17:21 +0000

    Prints "Hello World!" for the first time using pyre
    git-svn-id: svn://danse.us/AbInitio@171 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/ovini/cgi/main.py
+++ espresso/ovini/cgi/main.py
@@ -11,10 +11,31 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+from pyre.applications.Script import Script as base
+
+class HelloApp(base):
+
+    def main(self):
+        print 'Content-type: text/plain'
+        print
+        print "Hello World!"
+        return
+
+    def __init__(self, name='hello1'):
+        super(HelloApp, self).__init__(name=name)
+        return
+
+# main
+if __name__ == "__main__":
+    app = HelloApp()
+    app.run()
+
+"""
 print 'Content-type: text/plain'
 print
 print 'Hello, Alex!'
+"""
 
 __date__ = "$Jul 19, 2009 9:05:08 AM$"
 
-
+# End of file
