4705d485725bbf330456b2c6fe69f8d3ec808486
Alexander Dementsov <dexity@gmail.com>
2009-07-19 22:26:43 +0000

    Added directory 'shots' to store some screenshots as the development progresses!
    git-svn-id: svn://danse.us/AbInitio@172 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/ovini/cgi/main.py
+++ espresso/ovini/cgi/main.py
@@ -11,6 +11,38 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+def main():
+
+
+    from opal.applications.WebApplication import WebApplication
+
+
+    class MainApp(WebApplication):
+
+
+        def __init__(self):
+            WebApplication.__init__(self, name='main')
+            return
+
+
+    app = MainApp()
+    return app.run()
+
+# main
+if __name__ == '__main__':
+    # invoke the application shell
+    try:
+
+        main()
+    except:
+        import traceback
+        import time
+        t = time.ctime()
+        messages = traceback.format_exc().split('\n')
+
+"""
+# Simple web application that prints "Hello World!" using pyre
+
 from pyre.applications.Script import Script as base
 
 class HelloApp(base):
@@ -29,8 +61,11 @@ class HelloApp(base):
 if __name__ == "__main__":
     app = HelloApp()
     app.run()
+"""
 
 """
+# A very basic web application
+
 print 'Content-type: text/plain'
 print
 print 'Hello, Alex!'
