479729583a3aa17497144d0b7f4c517badb78f5d
Alexander Dementsov <dexity@gmail.com>
2009-08-05 07:19:14 +0000

    Added files to config directory
    git-svn-id: svn://danse.us/AbInitio@233 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ espresso/vinil/config/Make.mm
@@ -0,0 +1,54 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2005  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PROJECT = vinil
+PACKAGE = config
+
+#--------------------------------------------------------------------------
+#
+
+all: export-config-files
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+
+EXPORT_DATAFILES = \
+	idd-harness.pml \
+	idd-pickler.odb \
+	idd.pml \
+	librarian.pml \
+	main.pml \
+	web-weaver.pml \
+        widget.lib \
+
+CP_F = cp -f
+EXPORT_DATA_PATH = $(EXPORT_ROOT)/$(PACKAGE)
+
+export-config-files:: 
+	mkdir -p $(EXPORT_DATA_PATH); \
+	for x in $(EXPORT_DATAFILES); do { \
+	  $(CP_F) $$x $(EXPORT_DATA_PATH)/ ; \
+        } done
+
+
+# version
+# $Id: Make.mm,v 1.1.1.1 2006-11-27 00:09:14 aivazis Exp $
+
+# End of file
