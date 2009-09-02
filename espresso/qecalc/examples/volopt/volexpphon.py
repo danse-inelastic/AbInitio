c7f94dbde8320f64c92cfd4209cbf63f03408663
Nikolay Markovskiy <markovskiy@gmail.com>
2009-09-02 04:47:28 +0000

    git-svn-id: svn://danse.us/AbInitio@310 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ espresso/qecalc/examples/volopt/volexpphon.py
@@ -0,0 +1,44 @@
+#import numpy
+import os
+from qephon import QEPhon
+#from task.writetopwscf import varnameValue, atomic_positions
+#from task.task import Task
+
+
+
+a_range = [ 5.7285141671438824, 5.7317877776644526, 5.7350667579521337, 5.7381421125337928, 5.7414536572621246, 5.7445384926803174, 5.7477444904967463, 5.7514802489352217, 5.755333850422792, 5.7589075115531303, 5.7629454999299998, 5.7666501568080868]
+c_a_range = [2.2090980266298565, 2.2097169512764472, 2.2103232326527733, 2.2111584911247455, 2.2117142863526338, 2.2125256870194541, 2.213190687501422, 2.2141090078689722, 2.214882104904242, 2.2159692795744084, 2.2165114564527064, 2.2174293230773712]
+
+qe = QEPhon('config.ini')
+indexRange = range(4,13,4)
+print indexRange
+for i in indexRange:
+    qe.structure.lattice.a = a_range[i]
+    qe.structure.lattice.c = c_a_range[i]*a_range[i]
+    qe.structure.lattice.printBase()
+    qe.structure.saveStructureToPWSCF()
+    qe.multiPhononTaskLauncher()
+
+#    varnameValue(task.pwscfInput, 'celldm(1)', a_range[i])
+#    varnameValue(task.pwscfInput, 'celldm(2)', a_range[i])
+#    varnameValue(task.pwscfInput, 'celldm(3)', c_a_range[i])
+#    geometry = ['Al       0.000000000   0.0000000000000000   0.000000000',
+#                'B        0.500000000   0.2886751345948129   '+str(c_a_range[i]/2.),
+#                'B        0.000000000   0.5773502691896257   '+str(c_a_range[i]/2.)]
+#    atomic_positions(task.pwscfInput, geometry)
+#    task.getLauncher()
+
+#    Pol, Omega, qPoints = task.getEstimator()
+#    polName = "polarizations"+str(i)+".txt"
+#    omegaName = "freqs"+str(i)+".txt"
+#    qPointsName = "qpoints"+str(i)+".txt"
+    cpMatdynModesCmdStr = "cp "+"matdyn.modes "+ "matdyn" + str(i) + ".modes"
+    os.system(cpMatdynModesCmdStr)
+#   save pol vectors, frequencies and qpoints:
+    #numpy.savetxt(polName, Pol)
+#    numpy.savetxt(omegaName, Omega)
+#    numpy.savetxt(qPointsName, qPoints)
+#   backup the current state
+    backupName = 'backup'+str(i)+'.tgz'
+    backupCmdStr = "tar -zcf" + backupName + " *  --exclude='*tgz' --no-recursion"
+    os.system(backupCmdStr)
