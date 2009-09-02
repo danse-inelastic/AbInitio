da8d746004c4e1de7064a4bea451d9f107685192
Nikolay Markovskiy <markovskiy@gmail.com>
2009-09-02 05:22:58 +0000

    git-svn-id: svn://danse.us/AbInitio@315 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/qecalc/examples/volexpphon/volexpphon.py
+++ espresso/qecalc/examples/volexpphon/volexpphon.py
@@ -5,7 +5,7 @@ a_range = [ 5.7285141671438824, 5.7317877776644526, 5.7350667579521337, 5.738142
 c_a_range = [2.2090980266298565, 2.2097169512764472, 2.2103232326527733, 2.2111584911247455, 2.2117142863526338, 2.2125256870194541, 2.213190687501422, 2.2141090078689722, 2.214882104904242, 2.2159692795744084, 2.2165114564527064, 2.2174293230773712]
 
 qe = QEPhon('config.ini')
-indexRange = range(4,13,4)
+indexRange = range(1,6,2)
 print indexRange
 for i in indexRange:
     qe.structure.lattice.a = a_range[i]
@@ -14,10 +14,8 @@ for i in indexRange:
     qe.structure.saveStructureToPWSCF()
     qe.multiPhononTaskLauncher()
 
-    cpMatdynModesCmdStr = "cp "+"matdyn.modes "+ "matdyn" + str(i) + ".modes"
+    cpMatdynModesCmdStr = "cp "+"matdyn.modes "+ "matdyn" + str((i+1)*2) + ".modes"
     os.system(cpMatdynModesCmdStr)
-
-    backupName = 'backup'+str(i)+'.tgz'
+    backupName = 'backup'+str((i+1)*2)+'.tgz'
     backupCmdStr = "tar -zcf" + backupName + " *  --exclude='*tgz' --no-recursion"
     os.system(backupCmdStr)
-
