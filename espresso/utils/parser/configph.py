1a727145bf9f5f3f75b0d950c99ec8aa7346f88b
Alexander Dementsov <dexity@gmail.com>
2009-09-02 06:58:25 +0000

    Added directory utils where qe scripts are located
    git-svn-id: svn://danse.us/AbInitio@317 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ espresso/utils/parser/configph.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2009  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+# Configuration parameters from INPUT_PW.html file
+# First value is default value
+
+# Namelist: CONTROL
+
+# Simple verification for namelists and cards:
+
+
+
+__date__ = "$Sep 2, 2009 10:49:46 AM$"
+
+
