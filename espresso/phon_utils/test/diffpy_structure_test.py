0ac4375fe10c2c827c6d061e86bedda74d4e98bd
Nikolay Markovskiy <markovskiy@gmail.com>
2009-09-02 03:55:21 +0000

    eliminated a few bugs in qestructure/qephon/launcher/property started documenting: created class diagram in DOC directory. Created README file in qecalc folder. Created direcory examples with three examples
    git-svn-id: svn://danse.us/AbInitio@308 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/phon_utils/test/diffpy_structure_test.py
+++ espresso/phon_utils/test/diffpy_structure_test.py
@@ -11,17 +11,23 @@ myAtoms = ['Mg', 'B', 'B']
 myLattice = Lattice()
 theta = -30.*math.pi/180.
 rotMatrix = [[math.cos(theta),-math.sin(theta),0],[math.sin(theta),math.cos(theta),0],[0,0,1]]
-myLattice.setLatPar(1., 1, 1.,gamma=60.0,baserot = rotMatrix)
+#myLattice.setLatPar(1., 1, 1.,gamma=120.0)#,baserot = rotMatrix)
 print myLattice.base
 a = 5.2
 c = 6.3
 base = [[1,0,0],[-1./2.,math.sqrt(3.)/2.,0.],[0,0,c/a]]
 myLattice.setLatBase(base)
 print myLattice.base
+print "baserot:"
+print myLattice.baserot
 
-myLattice.setLatBase(numpy.array(base)*a)
-print myLattice
-print myLattice.abcABG()
+
+myLattice.setLatPar(5.2, 5.2, 6.3,gamma=120.0,baserot = myLattice.baserot)
+print myLattice.base
+
+#myLattice.setLatBase(numpy.array(base)*a)
+#print myLattice
+#print myLattice.abcABG()
 
 myStruct = Structure(myAtoms, myLattice)
 
