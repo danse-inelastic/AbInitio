6470b9279a02862d1fdb09ccc7bbef372b1ae4fc
Alexander Dementsov <dexity@gmail.com>
2009-09-26 15:37:49 +0000

    Removed javascript/luban
    git-svn-id: svn://danse.us/AbInitio@374 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/vinil/vinil/utils/Make.mm
+++ espresso/vinil/vinil/utils/Make.mm
@@ -17,25 +17,66 @@ PACKAGE = utils
 #--------------------------------------------------------------------------
 #
 
-all: export
+#EXPORT_PYTHON_MODULES = \
 
-#--------------------------------------------------------------------------
-# export
-
-EXPORT_PYTHON_MODULES = \
-        card.py \
-        configPW.py \
+EXPORT_DATAFILES = \
         dos.py \
-        namelist.py \
-        orderedDict.py \
         plot.py \
         pw.py \
-        qeconfig.py \
         utils.py \
 	__init__.py \
 
+EXPORT_DATADIRS = \
+        parser \
 
-export:: export-package-python-modules
+
+#export:: export-package-python-modules
+
+all: export-package-data
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+RSYNC_A = rsync -a
+EXPORT_DATA_PATH = $(EXPORT_ROOT)/$(PACKAGE)
+
+export-package-data:: export-package-datadirs export-package-datafiles
+
+
+export-package-datadirs:: $(EXPORT_DATADIRS) init-data-dirs
+	mkdir -p $(EXPORT_DATA_PATH); \
+	for x in $(EXPORT_DATADIRS); do { \
+            if [ -d $$x ]; then { \
+	        $(RSYNC_A) $$x $(EXPORT_DATA_PATH)/ ; \
+            } fi; \
+        } done
+
+
+export-package-datafiles:: $(EXPORT_DATAFILES)
+	for x in $(EXPORT_DATAFILES); do { \
+            if [ -f $$x ]; then { \
+	        $(RSYNC_A) $$x $(EXPORT_DATA_PATH)/$$x ; \
+            } fi; \
+        } done
+
+
+#initialize some data directories when necessary
+#if EXPORT_DATA_PATH/<subdir> already exists, skip; other wise, copy <subdir> over.
+init-data-dirs:
+	mkdir -p $(EXPORT_DATA_PATH); \
+	for x in $(INIT_DATADIRS); do { \
+            if [ -d $$x -a ! -d $(EXPORT_DATA_PATH)/$$x ]; then { \
+	        $(RSYNC_A) $$x $(EXPORT_DATA_PATH)/ ; \
+            } fi; \
+        } done
+
 
 # version
 # $Id$
