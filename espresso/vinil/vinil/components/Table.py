16d078269225cec3d9a1147c4f2d3b4615b3d9cc
Alexander Dementsov <dexity@gmail.com>
2009-10-06 00:25:19 +0000

    Table class that will handle tables in VNF
    git-svn-id: svn://danse.us/AbInitio@447 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ espresso/vinil/vinil/components/Table.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2009  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+class Table:
+    pass
+
+__date__ = "$Oct 5, 2009 5:24:07 PM$"
+
+
