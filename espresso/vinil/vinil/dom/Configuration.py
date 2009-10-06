c8fb386cae02a12dca86e3bb4d6590b64f4f924a
Alexander Dementsov <dexity@gmail.com>
2009-10-06 20:38:01 +0000

    Updated Configuration class
    git-svn-id: svn://danse.us/AbInitio@456 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/vinil/vinil/dom/Configuration.py
+++ espresso/vinil/vinil/dom/Configuration.py
@@ -18,22 +18,42 @@ class Configuration(Table):
     name = "configuration"
     import pyre.db
 
-    id = pyre.db.varchar(name="id", length=10)
+    id = pyre.db.varchar(name="id", length=8)
     id.constraints = 'PRIMARY KEY'
     id.meta['tip'] = "the unique id"
 
+    simulationId    = pyre.db.varchar(name="simulationId", length=8)
+    simulationId.constraints = 'REFERENCES simulation (id)'
+    simulationId.meta['tip'] = "simulationId"
 
+    name = pyre.db.varchar(name="name", length=1024, default='')
+    name.meta['tip'] = "name"
+
+    description = pyre.db.varchar(name="description", length=1024, default='')
+    description.meta['tip'] = "description"
+
+    date = pyre.db.varchar(name="date", length=16, default='')
+    date.meta['tip'] = "date"
+
+    text = pyre.db.varchar(name="text", length=8192, default='')
+    text.meta['tip'] = "text"
+
+
+"""
+# For debugging
 def inittable(db):
     def configuration(id):
         r           = Configuration()
-        r.id        = id
+        r.id        = params['id']
+        r.simulationId  = params['simulationId']
         return r
 
     records = [
-        configuration( 1 )
+        configuration( {"id": 1, "simulationId": 1} )
         ]
     for r in records: db.insertRow( r )
     return
+"""
 
 __date__ = "$Oct 5, 2009 8:58:32 AM$"
 
