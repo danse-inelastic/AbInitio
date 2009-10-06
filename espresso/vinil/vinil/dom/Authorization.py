839cd732182e5ed9a8a425ac115350d5f4dee194
Alexander Dementsov <dexity@gmail.com>
2009-10-06 20:59:50 +0000

    Updated Authorization class
    git-svn-id: svn://danse.us/AbInitio@459 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/vinil/vinil/dom/Authorization.py
+++ espresso/vinil/vinil/dom/Authorization.py
@@ -11,6 +11,48 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+from pyre.db.Table import Table
+
+class Server(Table):
+
+    name = "server"
+    import pyre.db
+
+    id = pyre.db.varchar(name="id", length=8)
+    id.constraints = 'PRIMARY KEY'
+    id.meta['tip'] = "the unique id"
+
+    serverId    = pyre.db.varchar(name="serverId", length=8)
+    serverId.constraints = 'REFERENCES server (id)'
+    serverId.meta['tip'] = "serverId"
+
+    userId    = pyre.db.varchar(name="userId", length=8)
+    userId.constraints = 'REFERENCES user (id)'
+    userId.meta['tip'] = "userId"
+
+    aname = pyre.db.varchar(name="aname", length=256)
+    aname.meta['tip'] = "aname"
+
+    description = pyre.db.varchar(name="description", length=1024, default='')
+    description.meta['tip'] = "description"
+
+
+"""
+# For debugging
+def inittable(db):
+    def server(params):
+        r           = Server()
+        r.id        = params['id']
+        r.serverId  = params['serverId']
+        r.userId    = params['userId']
+        return r
+
+    records = [
+        server( {"id": 1, "serverId": 1, "userId": 1})
+        ]
+    for r in records: db.insertRow( r )
+    return
+"""
 
 __date__ = "$Oct 6, 2009 12:18:36 PM$"
 
