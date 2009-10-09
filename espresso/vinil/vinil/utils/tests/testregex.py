0362df88b2f7df4313511da71d3e642b6937f171
Alexander Dementsov <dexity@gmail.com>
2009-10-09 01:05:49 +0000

    Implemented parsing of namelists. Seems to works fine but need more testing!
    git-svn-id: svn://danse.us/AbInitio@488 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/vinil/vinil/utils/tests/testregex.py
+++ espresso/vinil/vinil/utils/tests/testregex.py
@@ -32,31 +32,103 @@ textSimple = """ 	&system ! blah
 
 textB = """ & system # ! comment
     ibrav=2, ! fignja vsja_kaja!
+    
+    celldm(1) =6.65, ! tozhe fignja
+
+/ blah!
+
+& systemB # ! comment
+    ibrav=2, ! fignja vsja_kaja!
     celldm(1) =6.65, ! tozhe fignja
 / blah!
 """
 
-comment     = '!.*'
-namelist    = '([a-zA-Z_]*).*'    # Extracts namelist name ()
-spaces      = '[ \t]*'          # Spaces and tabs
-newline     = '[\n\r]*'         # New line ()
+textC = """
+ &control
+    calculation='scf'
+    restart_mode='from_scratch',
+    tprnfor = .true.
+    prefix='ni',
+    pseudo_dir = '',
+    outdir=''
+ /
+ &system
+    ibrav=2,
+    celldm(1) =6.65,
+    nat=  1,
+    ntyp= 1,
+    nspin=2,
+    starting_magnetization(1)=0.5,
+    degauss=0.02,
+    smearing='gauss',
+    occupations='smearing',
+    ecutwfc =27.0
+    ecutrho =300.0
+ /
+ &electrons
+    conv_thr =  1.0d-8
+    mixing_beta = 0.7
+ /
+"""
 
-def parser(s):
-    p   = re.compile(comment)
-    s1  = re.sub(p, '', s)      # Remove comments
-    patstr  = '%s&%s%s([\s\w,.=()]*)/' % (spaces, spaces, namelist)
-    p2  = re.compile(patstr)
-    m   = p2.match(s1)
 
-    s2  = m.group(2)            # Namelist content
-    s3  = re.sub(newline, '', s2)   # Remove empty new lines
+COMMENT     = '!.*'                 # Comment
+NAME        = '([a-zA-Z_]*)[^/]'    # Extracts namelist name ()
+SPACES      = '[ \t]*'              # Spaces and tabs
+NO_SPACES   = '[^\s]*'              # No spaces
+NEWLINE     = '[\n\r]*'             # New line ()
+PARAMTER    = '[\w,()]+'            # Parameter characters (space is not allowed)
+VALUE       = '[^\s,]+'              # Parameter's value (numerate all possible characters)
+EXPRESSION  = '(%s%s=%s%s)' % (PARAMTER, SPACES, SPACES, VALUE) # Parameter's expression
+NAMELIST    = """%s&%s%s([^/]*)/""" % (SPACES, SPACES, NAME) # Namelist block
 
+def parser(text):
+    nl  = parseNamelists(text)
+    parseCards(text)
+    print nl
 
-    print s3
+def parseNamelists(text):
+    namelists = []
+    p   = re.compile(COMMENT)
+    s1  = re.sub(p, '', text)       # Remove comments
+    p2  = re.compile(NAMELIST)
+    matches   = p2.findall(s1) #match(s1)
+    for m in matches:
+        name    = m[0]
+        params  = parseParams(m[1])
+        namelists.append((name, params))
 
+    return namelists
+
+def parseParams(text):
+    params  = []
+    p   = re.compile(EXPRESSION)    # Match expression
+    matches = p.findall(text)
+    for m in matches:
+        pl  = getParams(m) # Parameters list
+        params.append(pl)
+
+    return params
+
+def getParams(text):
+    """ Takes string like 'a = 2' and returns tuple ('a', 2) """
+    s = text.split('=')
+    for i in range(len(s)):
+        s[i] = s[i].strip()
+
+    param   = s[0]
+    val     = s[1]
+    # Assume that there are two values only: (variable, value) pair
+    assert len(s) == 2
+    
+    return (param, val)
+
+def parseCards(text):
+    #print "Parsing Cards"
+    return
 
 if __name__ == "__main__":
-    parser(textB)
+    parser(textC)
 
 __date__ = "$Oct 8, 2009 11:52:34 AM$"
 
