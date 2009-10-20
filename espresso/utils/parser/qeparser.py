65579e62e4554e26e833d4c6c95b8c1bad4a04d6
Alexander Dementsov <dexity@gmail.com>
2009-10-20 03:18:00 +0000

    Fixed bug with matching p.match(), can return None
    git-svn-id: svn://danse.us/AbInitio@570 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/utils/parser/qeparser.py
+++ espresso/utils/parser/qeparser.py
@@ -139,20 +139,21 @@ class QEParser:
         for l in rawlist:
             p   = re.compile(CARD)
             m   = p.match(l)
-            firstPart   = m.group(1).lower()
-            secondPart  = m.group(2).strip().lower()    # Catch argument of the card
-            if firstPart in self.cardRef:
-                cardName    = firstPart
-                cards[cardName]    = {}
+            if m is not None:
+                firstPart   = m.group(1).lower()
+                secondPart  = m.group(2).strip().lower()    # Catch argument of the card
+                if firstPart in self.cardRef:
+                    cardName    = firstPart
+                    cards[cardName]    = {}
 
-                if (secondPart != ''):
-                    cards[cardName]["args"] = secondPart
-                else:
-                    cards[cardName]["args"] = None
-                cards[cardName]["values"]   = []
+                    if (secondPart != ''):
+                        cards[cardName]["args"] = secondPart
+                    else:
+                        cards[cardName]["args"] = None
+                    cards[cardName]["values"]   = []
 
-            elif cardName is not None:
-                cards[cardName]["values"].append(l)
+                elif cardName is not None:
+                    cards[cardName]["values"].append(l)
 
         return cards
 
@@ -234,14 +235,69 @@ blah
 
 """
 
+textProblem = """
+ &control
+   calculation='scf'
+!    restart_mode='from_scratch',
+   wf_collect = .true.,
+   tstress = .true. ,
+   tprnfor = .true. ,
+   verbosity = 'high',
+   prefix='mgb2',
+   pseudo_dir = '/home/markovsk/projects/pslib/espresso/mgb2/',
+   lkpoint_dir = .false. ,
+   outdir='temp/'
+/
+&system
+   ibrav=4,
+   celldm(1) = 5.78739785,
+   celldm(2) = 5.78739785,
+   celldm(3) = 1.135794331,
+!    celldm(1) = 5.8260,
+!    celldm(2) = 5.8260,
+!    celldm(3) = 1.1420,
+   nat  =  3,
+   ntyp  = 2,
+   nspin = 1,
+   nbnd = 12,
+   occupations='smearing',
+!   degauss=0.025
+   degauss=0.025,
+   smearing = 'methfessel-paxton' ,
+   ecutwfc =32.0,
+   ecutrho =256.0,
+   la2f = .false.
+/
+&electrons
+   conv_thr =  1.0d-12
+   diago_full_acc=.TRUE.
+/
+
+ATOMIC_SPECIES
+  Mg  24.305 mg_6.ncpp
+  B   11.000 B.pbe-n-van_ak.UPF
+!b_rc_1.4_pcc.ncpp
+!  B   10.811 B.pbe-tmnc.UPF
+
+
+ATOMIC_POSITIONS alat
+Mg       0.000000000   0.0000000000000000   0.000000000
+B        0.500000000   0.2886751345948129   0.5678971655
+B        0.000000000   0.5773502691896257   0.5678971655
+
+
+K_POINTS AUTOMATIC
+24 24 24 0 0 0
+
+"""
 
 if __name__ == "__main__":
-    qeparserText    = QEParser(configText = textPW)
+    qeparserText    = QEParser(configText = textProblem)
     qeparserText.parse()
     qeparserText.toString()
-    qeparserFile    = QEParser(filename = "../tests/ni.scf.in")
-    qeparserFile.parse()
-    qeparserFile.toString()
+#    qeparserFile    = QEParser(filename = "../tests/ni.scf.in")
+#    qeparserFile.parse()
+#    qeparserFile.toString()
 
 
 __date__ = "$Oct 9, 2009 4:34:28 PM$"
