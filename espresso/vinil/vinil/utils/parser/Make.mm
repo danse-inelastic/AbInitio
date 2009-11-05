6429012dd92e4323de7f74cdf6ef4ac1d5264c2d
Alexander Dementsov <dexity@gmail.com>
2009-11-05 19:57:38 +0000

    Added parser directory to vinil/utils/
    git-svn-id: svn://danse.us/AbInitio@825 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ espresso/vinil/vinil/utils/parser/Make.mm
@@ -0,0 +1,53 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Michael A.G. Aivazis
+#                        California Institute of Technology
+#                        (C) 1998-2005  All Rights Reserved
+#
+# <LicenseText>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PROJECT = vinil
+PACKAGE = utils/parser
+
+
+#--------------------------------------------------------------------------
+#
+
+EXPORT_PYTHON_MODULES = \
+        card.py \
+        namelist.py \
+        orderedDict.py \
+        qeinput.py \
+        qeparser.py \
+        __init__.py \
+
+BUILD_DIRS = \
+        inputs \
+
+OTHER_DIRS = \
+
+RECURSE_DIRS = $(BUILD_DIRS) $(OTHER_DIRS)
+
+all: export
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+export:: export-package-python-modules
+	BLD_ACTION="export" $(MM) recurse
+
+
+# version
+# $Id$
+
+# End of file
