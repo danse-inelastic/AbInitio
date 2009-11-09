40f148e4318c8561ecaf2648a4bc377ce2ae104b
Alexander Dementsov <dexity@gmail.com>
2009-11-09 23:58:20 +0000

    Generates simulation chain with input files
    git-svn-id: svn://danse.us/AbInitio@848 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/vinil/vinil/utils/simchain.py
+++ espresso/vinil/vinil/utils/simchain.py
@@ -11,36 +11,89 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+from vinil.utils.const import SIMULATIONS
+
 from luban.content.Splitter import Splitter
 from luban.content.Paragraph import Paragraph
 from luban.content import load
 from luban.content.Link import Link
 
 
-class SimChain():
+class SimChain:
 
-    def __init__(self, director):
+    def __init__(self, director, type=None):
         self._director  = director
+        self._simtype   = type
+        self._simlist   = self._getSimlist(type)
+
 
     def chain(self, id):
-        config  = self._director.clerk.getConfigurations(where="simulationId='%s'" % id)
-        if config:
-            filename    = config[0].filename
-        else:
-            filename    = "Add"
+        inputs          = self._director.clerk.getConfigurations(where="simulationId='%s'" % id)
+        orderedInputs   = self._orderInput(self._simlist, inputs)
 
         splitter    = Splitter(orientation='horizontal')
-        one     = splitter.section()
-        one.add(Paragraph(text="PW "))
-        one.add(Link(label=filename, Class="action-link", onclick=load(actor="espresso-set-config", routine="link", id=id)))
-        sep     = splitter.section()
-        sep.add(Paragraph(text=" ----> "))
-        two     = splitter.section()
-        two.add(Paragraph(text="DOS"))
-        two.add(Link(label="Add"))
+        listsize    = len(self._simlist)
+
+        for i in range(listsize):
+            section     = splitter.section()
+            section.add(Paragraph(text=self._simlist[i]))
+            section.add(Link(label=self._inputText(orderedInputs[i]), Class="action-link", onclick=load(actor="espresso-set-config", routine="link", id=id)))
+
+            if i != listsize - 1:   # No arrow for last config
+                sep     = splitter.section()        # Separator
+                sep.add(Paragraph(text=" ----> "))
 
         return splitter
-    
+
+
+    def _inputText(self, input):
+        if input:
+            return input.filename
+
+        return "Add"
+
+
+    def _orderInput(self, simlist, inputs):
+        """E.g. simlist = ("PW", "PH")"""
+        newinputs   = []
+
+        for name in simlist:
+            newinputs.append(self._simObject(name, inputs))
+
+        return newinputs
+
+
+    def _simObject(self, name, inputs):
+        """Returns object if simulation type exists or None otherwise"""
+        for sim in inputs:
+            if sim.type == name:
+                return sim
+
+        return None
+
+
+    def _getSimlist(self, type):
+        if type in SIMULATIONS:
+            return SIMULATIONS[type]
+
+        return ()
+
+
+if __name__ == "__main__":
+    chain   = SimChain(None, None)
+
+
 __date__ = "$Nov 9, 2009 10:50:54 AM$"
+
+
+# *************** DEAD CODE ********************
+#        one     = splitter.section()
+#        one.add(Paragraph(text="PW "))
+#        one.add(Link(label=filename, Class="action-link", onclick=load(actor="espresso-set-config", routine="link", id=id)))
+#        sep     = splitter.section()        # Separator
+#        sep.add(Paragraph(text=" -> "))
+#        two     = splitter.section()
+#        two.add(Paragraph(text="DOS"))
+#        two.add(Link(label="Add"))
 
 
