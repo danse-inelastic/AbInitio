57644ac05c5f74829f3e8ee1125a9d4a24d43ba2
Alexander Dementsov <dexity@gmail.com>
2010-02-28 21:36:55 +0000

    Adding stubs for a new job manager
    git-svn-id: svn://danse.us/AbInitio@1120 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ espresso/lab/hydra/tasktracker.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2009  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+
+class TaskTracker:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def offerService(self):
+        pass
+
+    
+
+__date__ = "$Feb 16, 2010 4:50:50 PM$"
+
+
