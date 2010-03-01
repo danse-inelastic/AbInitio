c91cea4c8f8ab24a3670ade7a7786984c2c63323
Alexander Dementsov <dexity@gmail.com>
2010-03-01 03:20:48 +0000

    Added interfaces to jobtracker and jobclient
    git-svn-id: svn://danse.us/AbInitio@1121 4efde5e4-ebd3-4001-8375-eeb568285340

---
+++ espresso/lab/hydra/jobclient.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                               Alex Dementsov
+#                      California Institute of Technology
+#                        (C) 2009  All Rights Reserved
+#
+# {LicenseText}
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+class JobClient:
+
+    def __init__(self):
+        pass
+
+    def init(self, conf):
+        pass
+
+    def close(self):
+        pass
+
+    def submitJob(self):
+        pass
+
+    def getJob(self, jobid):
+        pass
+
+    def getTaskReports(self):
+        pass
+
+    def getClusterStatus(self):
+        pass
+
+    def getAllJobs(self):
+        pass
+
+    def runJob(self, job):
+        pass
+
+    def run(self):
+        pass
+
+    def getQueues(self):
+        pass
+
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+
+
+__date__ = "$Feb 28, 2010 2:43:55 PM$"
+
+
--- espresso/lab/hydra/jobtracker.py
+++ espresso/lab/hydra/jobtracker.py
@@ -11,6 +11,139 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 
+#STATE = (INITIALIZING, RUNNING)
+
+class JobTracker:
+
+    def __init__(self):
+        pass
+
+    def startTracker(self):
+        pass
+
+    def stopTracker(self):
+        pass
+
+    def hasRestarted(self):
+        pass
+
+    def hasRecovered(self):
+        pass
+
+    def getTrackerPort(self):
+        pass
+
+    def runningJobs(self):
+        pass
+
+    def getRunningJobs(self):
+        pass
+
+    def failedJobs(self):
+        pass
+
+    def completedJobs(self):
+        pass
+
+    def taskTrackers(self):
+        pass
+
+    def activeTaskTrackers(self):
+        pass
+
+    def getTaskTracker(self, trackerID):
+        pass
+
+    def getNewJobId(self):
+        pass
+
+    def heartbeat(self):
+        pass
+
+    def offerService(self):
+        pass
+
+    def submitJob(self):
+        pass
+
+    def getClusterStatus():
+        pass
+
+    def killJob(self):
+        pass
+
+    def initJob(self):
+        pass
+
+    def failJob(self):
+        pass
+
+    def getJobStatus(self):
+        pass
+
+    def getTaskReports(self):
+        pass
+
+    def killTask(self):
+        pass
+
+    def getAllJobs(self):
+        pass
+
+    def getJob(self, jobid):
+        pass
+
+    def getQueues(self):
+        pass
+
+    def run(self):
+        pass
+
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+#
+#    def (self):
+#        pass
+
 
 __date__ = "$Feb 16, 2010 4:50:28 PM$"
 
