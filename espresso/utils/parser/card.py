eef9ff71621d5966bb2b90791e80d05f7556557e
Nikolay Markovskiy <markovskiy@gmail.com>
2010-05-17 23:02:38 +0000

    fixed indent bug in card.toString
    git-svn-id: svn://danse.us/AbInitio@1184 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/utils/parser/card.py
+++ espresso/utils/parser/card.py
@@ -71,7 +71,7 @@ class Card():
             s += br
 
         for l in self._lines:
-            s += '%s%s%s' % (indent, l.strip().strip(br), br)
+            s += '%s%s%s' % (indent, l.strip().strip('\n').strip(br), br)
 
         return s
 
