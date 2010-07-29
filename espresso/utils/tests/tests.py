32b18e037bc01e4cfe14a15288db5293cb22c4bf
Alexander Dementsov <dexity@gmail.com>
2010-07-29 19:44:44 +0000

    Added fixtures for qeparser unit tests
    git-svn-id: svn://danse.us/AbInitio@1224 4efde5e4-ebd3-4001-8375-eeb568285340

--- espresso/utils/tests/tests.py
+++ espresso/utils/tests/tests.py
@@ -10,15 +10,17 @@
 #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
+"""
+Unit tests for the following classes (modules):
+    Namelist, Card, QEParser, QEInput
+"""
 
-# Before to run tests, please add "../parser" to PYTHON_PATH environmental variable
-
+import unittest
+import fixtures
 from namelist import Namelist
 from card import Card
 from qeparser import QEParser
 from qeinput import QEInput
-
-import unittest
 
 class QEParserTest(unittest.TestCase):
 
@@ -32,6 +34,9 @@ class QEParserTest(unittest.TestCase):
         nl  = Namelist("control")
         self.assertEqual(nl.name(), "control")
 
+    def test_hello(self):
+        print fixtures.textCards
+
 
 if __name__ == '__main__':
     unittest.main()
@@ -42,121 +47,6 @@ if __name__ == '__main__':
 # qeinput
 
 
-
-#textPW = """
-# &control
-#    calculation='scf'
-#    restart_mode='from_scratch',
-#    tprnfor = .true.
-#    prefix='ni',
-#    pseudo_dir = '',
-#    outdir=''
-# /
-# &system
-#    ibrav=2,
-#    celldm(1) =6.65,
-#    nat=  1,
-#    ntyp= 1,
-#    nspin=2,
-#    starting_magnetization(1)=0.5,
-#    degauss=0.02,
-#    smearing='gauss',
-#    occupations='smearing',
-#    ecutwfc =27.0
-#    ecutrho =300.0
-# /
-# &electrons
-#    conv_thr =  1.0d-8
-#    mixing_beta = 0.7
-# /
-#
-#
-#ATOMIC_SPECIES
-# Ni  26.98  Ni.pbe-nd-rrkjus.UPF
-#
-#ATOMIC_POSITIONS
-# Ni 0.00 0.00 0.00
-#K_POINTS AUTOMATIC
-#4 4 4 1 1 1
-#blah
-#
-#"""
-#
-#textCards = """
-#ATOMIC_POSITIONS
-# Ni 0.00 0.00 0.00
-#K_POINTS AUTOMATIC
-#4 4 4 1 1 1
-#blah
-#
-#"""
-#
-#textProblem = """
-#CELL_PARAMETERS
-#   0.993162743  -0.000000000   0.000000000
-#  -0.496581371   0.860104165  -0.000000000
-#  -0.000000000  -0.000000000   4.345938530
-#ATOMIC_POSITIONS
-# Ni 0.00 0.00 0.00
-#K_POINTS AUTOMATIC
-#4 4 4 1 1 1
-#blah
-#
-#"""
-#
-#textMatdyn = """
-# &input
-#    asr='crystal',
-#    amass(1)=24.305, amass(2)=11.000,
-#    flfrc='mgb2666.fc'
-# /
-#176
-#0.000000    0.000000    0.456392    0.000000
-#0.000000    0.000000    0.447264    0.009128
-#0.000000    0.000000    0.438137    0.018256
-#0.000000    0.000000    0.429009    0.027384
-#0.000000    0.000000    0.419881    0.036511
-#"""
-#
-#textDynmat = """
-#&input  fildyn='mgb2.dynG', asr='simple',
-#        q(1)=0.0, q(2)=0.0, q(3)=0.0 /
-#"""
-#
-#textPh  = """
-# &inputph
-#  tr2_ph=1.0d-10,
-#  amass(1)=24.305,
-#  amass(2)=11.000,
-#  prefix='mgb2',
-#  outdir='/scratch/markovsk/mgb2'
-#  fildyn='mgb2.dynG',
-# /
-#
-#"""
-#
-#textHeader  = """
-#&INPUTPH
-#   tr2_ph = 1.0d-12,
-#   prefix = 'si',
-#   epsil = .false.,
-#   trans = .true.,
-#   zue = .false.,
-#   outdir = '/scratch/si',
-#   amass(1) = 28.0855,
-#   fildyn = 'si.dyn_G',
-#   fildrho = 'si.drho_G',
-#/
-#0.0 0.0 0.0
-#"""
-#
-## This is not a problem text (just add spaces between commas)
-#textComma   = """&input
-#   asr='crystal',  dos=.true.
-#   amass(1)=26.982538, amass(2)=11.000,
-#   flfrc='mgalb4666.fc', fldos='mgalb4.666.phdos', nk1=28,nk2=28,nk3=28
-#/
-#"""
 #
 #def testMatdyn():
 #    parser    = QEParser(configText = textMatdyn, type="matdyn")
